2c97d20a09f2972762273d0bd8c32f6665b33590
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-10-01 13:46:10 -0500

    The Third component was created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 
 import './App.css'
-import { FirstComponent } from './components/FirstComponent'
-import { SecondComponent } from './components/SecondComponent'
+// import { FirstComponent } from './components/FirstComponent'
+// import { SecondComponent } from './components/SecondComponent'
+import { ThirdComponent } from './components/ThirdComponent';
 
 function App() {
 
+
+
   return (
     <>
-      <FirstComponent/>
-      <SecondComponent/>
+      <div className="App">
+        <header className="App-header">
+    
+          <hr className='white-hr'/>
+        {/* <FirstComponent /> */}
+        <hr className='white-hr'/>
+        {/* <SecondComponent /> */}
+        <hr className='white-hr'/>
+        <ThirdComponent
+          name="Kerly"
+          last_name="Sarrias"
+          age="23"
+        />
+        <hr className='white-hr'/>
+
+        <hr className='white-hr'/>
+        </header>
+      </div>
     </>
   )
 }
