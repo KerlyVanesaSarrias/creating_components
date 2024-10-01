111e74303f400e28f608446fdb226034a30271f0
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-10-01 14:02:05 -0500

    add handle change name

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@
 import { useState } from 'react';
 import './App.css'
 import { Child } from './components/Child';
-// import { FirstComponent } from './components/FirstComponent'
-// import { SecondComponent } from './components/SecondComponent'
-import { ThirdComponent } from './components/ThirdComponent';
+
 
 function App() {
 
@@ -14,17 +12,6 @@ function App() {
     <>
       <div className="App">
         <header className="App-header">
-    
-          <hr className='white-hr'/>
-        {/* <FirstComponent /> */}
-        <hr className='white-hr'/>
-        {/* <SecondComponent /> */}
-        <hr className='white-hr'/>
-        <ThirdComponent
-          name="Kerly"
-          last_name="Sarrias"
-          age="23"
-        />
         <hr className='white-hr'/>
         <Child 
           name={name}
