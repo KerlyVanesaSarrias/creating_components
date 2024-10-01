d07669ddc45e59d96842bd2e0459b1995da487d5
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-10-01 13:52:53 -0500

    add component Child

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
+import { useState } from 'react';
 import './App.css'
+import { Child } from './components/Child';
 // import { FirstComponent } from './components/FirstComponent'
 // import { SecondComponent } from './components/SecondComponent'
 import { ThirdComponent } from './components/ThirdComponent';
 
 function App() {
 
-
+  const [name, setName] = useState("Kerly Vanessa Sarrias");
 
   return (
     <>
@@ -24,7 +26,10 @@ function App() {
           age="23"
         />
         <hr className='white-hr'/>
-
+        <Child 
+          name={name}
+          setName={setName} 
+        />
         <hr className='white-hr'/>
         </header>
       </div>
